2e7187df215de44acafe9a26518cf206a4586e5a
kalifun <37646342+kalifun@users.noreply.github.com>
2023-01-30 19:58:22 +0800

    Update neo-tree.lua

--- .config/nvim/lua/plugins/editor/neo-tree.lua
+++ .config/nvim/lua/plugins/editor/neo-tree.lua
@@ -25,6 +25,14 @@ return {
 	-- end,
 	init = function()
 		vim.g.neo_tree_remove_legacy_commands = 1
+		vim.fn.sign_define("DiagnosticSignError",
+			{ text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn",
+			{ text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo",
+			{ text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint",
+			{ text = "", texthl = "DiagnosticSignHint" })
 		if vim.fn.argc() == 1 then
 			local stat = vim.loop.fs_stat(vim.fn.argv(0))
 			if stat and stat.type == "directory" then
