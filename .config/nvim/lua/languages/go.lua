90f3e827afa9f8fc91ab19c90ba50decab0dffd3
kalifun <37646342+kalifun@users.noreply.github.com>
2023-01-31 23:48:02 +0800

    Create go.lua

---
+++ .config/nvim/lua/languages/go.lua
@@ -0,0 +1,11 @@
+return {
+    "ray-x/go.nvim",
+    dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter"
+    },
+    config = function ()
+        require("go").setup()
+    end
+}
