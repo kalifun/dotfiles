007923deef7baa03f2b75f95487dd7be4fe52bad
kalifun <37646342+kalifun@users.noreply.github.com>
2023-02-03 15:16:35 +0800

    Update lualine.lua

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -7,4 +7,31 @@ return {
 			disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
 		},
 	},
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.status.message.get_hl,
+						cond = require("noice").api.status.message.has,
+					},
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.search.get,
+						cond = require("noice").api.status.search.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
