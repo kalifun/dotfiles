dfc66ecf77c4205a351f33260175f740db9a2723
kalifun <kalifun@163.com>
2023-03-27 22:40:25 +0800

    Update nvim-cmp.lua

--- .config/nvim/lua/plugins/coding/nvim-cmp.lua
+++ .config/nvim/lua/plugins/coding/nvim-cmp.lua
@@ -7,12 +7,45 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "saadparwaiz1/cmp_luasnip",
+    "f3fora/cmp-spell",
+    "hrsh7th/cmp-cmdline",
+    "petertriho/cmp-git",
   },
   opts = function()
     local cmp = require("cmp")
+    
     -- autopairs setup
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    -- git commit completion
+    cmp.setup.filetype("gitcommit", {
+      sources = cmp.config.sources({
+        { name = "cmp_git" },   -- You can specify the `cmp_git` source if you were installed it.
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    -- command line completion
+    --
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+
     return {
       completion = {
         completeopt = "menu,menuone,noinsert",
