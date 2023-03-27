39c2ab89bcf565eccccd288b71e6bf57f20e53de
kalifun <37646342+kalifun@users.noreply.github.com>
2023-03-27 20:06:40 +0800

    Update util.lua

--- .config/nvim/lua/libs/util.lua
+++ .config/nvim/lua/libs/util.lua
@@ -65,4 +65,30 @@ function M.telescope(builtin, opts)
     end
 end
 
+---@param plugin string
+function M.has(plugin)
+  return require("lazy.core.config").plugins[plugin] ~= nil
+end
+
+---@param name string
+function M.opts(name)
+  local plugin = require("lazy.core.config").plugins[name]
+  if not plugin then
+    return {}
+  end
+  local Plugin = require("lazy.core.plugin")
+  return Plugin.values(plugin, "opts", false)
+end
+
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
 return M
