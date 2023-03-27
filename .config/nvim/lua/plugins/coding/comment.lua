ce2738721ee97e70242dd2b812fdf1f064d8dda5
kalifun <kalifun@163.com>
2023-03-27 22:15:03 +0800

    add plugins

---
+++ .config/nvim/lua/plugins/coding/comment.lua
@@ -0,0 +1,8 @@
+return {
+	"numToStr/Comment.nvim",
+	event = "VeryLazy",
+	-- lazy = true,
+	config = function()
+		require("Comment").setup()
+	end,
+}
--- .config/nvim/lua/plugins/ui/bufferline.lua
+++ .config/nvim/lua/plugins/ui/bufferline.lua
@@ -12,7 +12,7 @@ return {
             diagnostics_indicator = function(_, _, diag)
                 local icons = require("lazyvim.config").icons.diagnostics
                 local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-                    .. (diag.warning and icons.Warn .. diag.warning or "")
+                    .. (diag.warning and icons.Warning .. diag.warning or "")
                 return vim.trim(ret)
             end,
             offsets = {
