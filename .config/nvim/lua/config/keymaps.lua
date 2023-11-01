9fe6345026425b805a3189d1d8c694ce6c6f41ed
kalifun <kalifun@163.com>
2023-11-01 19:54:58 +0800

    feat: keymaps and opts

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,70 +1,88 @@
--- lazy.nvim
-vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+return {
+    -- lazy.nvim
+    { "n",          "<leader>l",    "<cmd>:Lazy<cr>",                   { desc = "Lazy" } },
 
--- better up/down
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+    -- better up/down
+    { "n",          "j",            "v:count == 0 ? 'gj' : 'j'",        { expr = true, silent = true } },
+    { "n",          "k",            "v:count == 0 ? 'gk' : 'k'",        { expr = true, silent = true } },
 
--- window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+    -- window navigation
+    { "n",          "<C-h>",        "<C-w>h",                           { desc = "Go to left window" } },
+    { "n",          "<C-j>",        "<C-w>j",                           { desc = "Go to lower window" } },
+    { "n",          "<C-k>",        "<C-w>k",                           { desc = "Go to upper window" } },
+    { "n",          "<C-l>",        "<C-w>l",                           { desc = "Go to right window" } },
 
--- window management
-vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "delete-window" })
-vim.keymap.set("n", "<l eeader>wj", "<C-W>s", { desc = "split-window-below" })
-vim.keymap.set("n", "<leader>wl", "<C-W>v", { desc = "split-window-right" })
+    -- window management
+    { "n",          "<leader>wd",   "<C-W>c",                           { desc = "delete-window" } },
+    { "n",          "<l eeader>wj", "<C-W>s",                           { desc = "split-window-below" } },
+    { "n",          "<leader>wl",   "<C-W>v",                           { desc = "split-window-right" } },
 
--- buffers navigation
-vim.keymap.set("n", "<leader>bd", "<cmd>:bdelete<cr>", { desc = "Delete Buffer" })
-vim.keymap.set("n", "<leader>b]", "<cmd>:BufferLineCycleNext<CR>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<leader>bb", "<cmd>:e #<cr>", { desc = "Switch to Other Buffer" })
-vim.keymap.set("n", "<leader>b[", "<cmd>:BufferLineCyclePrev<CR>", { desc = "Previous Buffer" })
+    -- buffers navigation
+    { "n",          "<leader>bd",   "<cmd>:bdelete<cr>",                { desc = "Delete Buffer" } },
+    { "n",          "<leader>b]",   "<cmd>:BufferLineCycleNext<CR>",    { desc = "Next Buffer" } },
+    { "n",          "<leader>bb",   "<cmd>:e #<cr>",                    { desc = "Switch to Other Buffer" } },
+    { "n",          "<leader>b[",   "<cmd>:BufferLineCyclePrev<CR>",    { desc = "Previous Buffer" } },
 
--- Clear search with <esc>
-vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+    -- Clear search with <esc>
+    { { "i", "n" }, "<esc>",        "<cmd>noh<cr><esc>",                { desc = "Escape and clear hlsearch" } },
 
--- Clear search and redraw
-vim.keymap.set("n", "<leader>r", "<cmd>noh<cr><cmd>redraw<cr><c-l>", { desc = "Redraw and clear hlsearch" })
+    -- Clear search and redraw
+    { "n",          "<leader>r",    "<cmd>noh<cr><cmd>redraw<cr><c-l>", { desc = "Redraw and clear hlsearch" } },
 
--- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-vim.keymap.set("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-vim.keymap.set("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+    -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+    { "n", "n", "'Nn'[v:searchforward]", {
+        expr = true,
+        desc = "Next search result"
+    } },
+    { "x", "n", "'Nn'[v:searchforward]", {
+        expr = true,
+        desc = "Next search result"
+    } },
+    { "o", "n", "'Nn'[v:searchforward]", {
+        expr = true,
+        desc = "Next search result"
+    } },
+    { "n", "N", "'nN'[v:searchforward]", {
+        expr = true,
+        desc = "Prev search result"
+    } },
+    { "x", "N", "'nN'[v:searchforward]", {
+        expr = true,
+        desc = "Prev search result"
+    } },
+    { "o", "N", "'nN'[v:searchforward]", {
+        expr = true,
+        desc = "Prev search result"
+    } },
 
--- better indenting
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+    -- better indenting
+    { "v",                    "<",          "<gv" },
+    { "v",                    ">",          ">gv" },
 
--- quit
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-
-
--- Move Lines
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-
-
--- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
-
--- save file
-vim.keymap.set({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-
-
--- new file
-vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+    -- quit
+    { "n",                    "<leader>qq", "<cmd>qa<cr>",                 { desc = "Quit all" } },
 
 
+    -- Move Lines
+    { "n",                    "<A-j>",      "<cmd>m .+1<cr>==",            { desc = "Move down" } },
+    { "n",                    "<A-k>",      "<cmd>m .-2<cr>==",            { desc = "Move up" } },
+    { "i",                    "<A-j>",      "<esc><cmd>m .+1<cr>==gi",     { desc = "Move down" } },
+    { "i",                    "<A-k>",      "<esc><cmd>m .-2<cr>==gi",     { desc = "Move up" } },
+    { "v",                    "<A-j>",      ":m '>+1<cr>gv=gv",            { desc = "Move down" } },
+    { "v",                    "<A-k>",      ":m '<-2<cr>gv=gv",            { desc = "Move up" } },
+
+
+    -- Resize window using <ctrl> arrow keys
+    { "n",                    "<C-Up>",     "<cmd>resize +2<cr>",          { desc = "Increase window height" } },
+    { "n",                    "<C-Down>",   "<cmd>resize -2<cr>",          { desc = "Decrease window height" } },
+    { "n",                    "<C-Left>",   "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" } },
+    { "n",                    "<C-Right>",  "<cmd>vertical resize +2<cr>", { desc = "Increase window width" } },
+
+
+    -- save file
+    { { "i", "v", "n", "s" }, "<C-s>",      "<cmd>w<cr><esc>",             { desc = "Save file" } },
+
+
+    -- new file
+    { "n",                    "<leader>fn", "<cmd>enew<cr>",               { desc = "New File" } },
+}
--- .config/nvim/lua/config/lazy-options.lua
+++
@@ -1,5 +0,0 @@
-return {
-    defaults = {
-        lazy = true,
-    },
-}
