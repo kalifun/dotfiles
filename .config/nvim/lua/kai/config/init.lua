349db8630a5820c66c6fb11a1b6c51626d3d2314
kalifun <kalifun@163.com>
2023-12-27 17:33:38 +0800

    #14 remove neovim configurations

--- .config/nvim/lua/kai/config/init.lua
+++
@@ -1,7 +0,0 @@
-local utils = require("kai.libs.utils")
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-utils.set_vim_opts(require("kai.config.options"))
-utils.set_keymaps(require("kai.config.keymaps"))
--- .config/nvim/lua/kai/plugins/coding/nvim-autopairs.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"windwp/nvim-autopairs",
-	event = "VeryLazy",
-	config = function(_, opts)
-		require("nvim-autopairs").setup(opts)
-	end,
-}
